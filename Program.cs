9679852d76bc03643f8cdaadfba7eedff6e07ed8
five <tknl45@hotmail.com>
2022-06-02 14:50:58 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.Drawing;
+using System.Drawing.Imaging;
+using PdfLibCore;
+using PdfLibCore.Enums;
+
+Console.WriteLine("Hello, World!");
+var dpiX = 200D;
+var dpiY = 200D;
+var filename = "s1";
+
+
+var pageNum = 0;
+using var pdfDocument = new PdfDocument(File.Open( $"{filename}.pdf", FileMode.Open));
+foreach (var page in pdfDocument.Pages)
+{   pageNum ++;
+    using var pdfPage = page;
+    var pageWidth = (int) (dpiX * pdfPage.Size.Width / 72);
+    var pageHeight = (int) (dpiY * pdfPage.Size.Height / 72);
+
+    using var bitmap = new PdfiumBitmap(pageWidth, pageHeight, true);
+    pdfPage.Render(bitmap, PageOrientations.Normal, RenderingFlags.LcdText);
+    using var stream = bitmap.AsBmpStream(dpiX, dpiY);
+
+    Image img = System.Drawing.Image.FromStream(stream);
+
+    img.Save($"{filename}-{pageNum}.jpg", ImageFormat.Jpeg);
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# C# pdf to jpg
+
+## step
+
+1. put .pdf file in root folder. ex. `s1.pdf`
+2. set `filename` in `Program.cs`. ex. `s1`
+3. execute cmd: dotnet run
+4. see the page files
